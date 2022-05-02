eb2beb5059852dc749245b17dafd14da8b7ea8d1
Арман Чархчян <re-mont1@mail.ru>
2022-05-02 13:56:44 +0400

    fix

--- Sources/DesignSystem/Extensions/ViewController/UIViewController + Extensions.swift
+++ Sources/DesignSystem/Extensions/ViewController/UIViewController + Extensions.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UIViewController {
-    var topBarHeight: CGFloat {
+    public var topBarHeight: CGFloat {
         return (view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0.0) + (self.navigationController?.navigationBar.frame.height ?? 0.0)
     }
     
-    var buttonBarHeight: CGFloat {
+    public var buttonBarHeight: CGFloat {
         return self.tabBarController?.tabBar.frame.height ?? 0.0
     }
 }
